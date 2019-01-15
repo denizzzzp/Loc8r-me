00764899c38c9f8ef37ea81136ec035753c590f8
denizzzzp <flint_sk8@ua.fm>
2019-01-15 04:43:10 +0200

    15.01.2019-2

---
+++ app_api/models/db.js
@@ -0,0 +1,57 @@
+var mongoose = require('mongoose');
+var gracefulShutdown;
+var dbURI = 'mongodb://192.168.21.251/Loc8r';
+//var logDB = mongoose.createConnection(dbURIlog);
+mongoose.connect(dbURI, {useNewUrlParser: true});
+
+// CONNECTION EVENTS
+mongoose.connection.on('connected', function() {
+    console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error', function(err) {
+    console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose disconnected');
+    mongoose.connect(dbURI, {useNewUrlParser: true});
+});
+
+// CAPTURE APP TERMINATION / RESTART EVENTS
+// To be called when process is restarted or terminated
+gracefulShutdown = function(msg, callback) {
+    mongoose.connection.close(function() {
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
+};
+// For nodemon restarts
+process.once('SIGUSR2', function() {
+    gracefulShutdown('nodemon restart', function() {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+// For app termination
+process.on('SIGINT', function() {
+    gracefulShutdown('app termination', function() {
+        process.exit(0);
+    });
+});
+// For Heroku app termination
+process.on('SIGTERM', function() {
+    gracefulShutdown('Heroku app termination', function() {
+        process.exit(0);
+    });
+});
+
+var readLine = require ("readline");
+if (process.platform === "win32"){
+  var rl = readLine.createInterface ({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.on ("SIGINT", function (){
+    process.emit ("SIGINT");
+  });
+}
+
+require('./locations');
